8cbbc1032c54e98cad7b7665ffffde1ad3321c54
Dianshu Liao <109912393+Dianshu-Liao@users.noreply.github.com>
2025-06-03 10:52:39 +1000

    Add files via upload

--- ProjectInfoExtraction/src/main/java/com/Config.java
+++ ProjectInfoExtraction/src/main/java/com/Config.java
@@ -12,7 +12,7 @@ public class Config {
     public static String projectclassDir = "/target/classes/";
 
 //    // for commons-codec
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/commons-codec-master";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-codec-master";
 //    public static String package_name = "org.apache.commons.codec";
 //    public static String projectSrcDir = "/src/main/java/org/apache/commons/codec";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/apache/commons/codec";
@@ -21,7 +21,7 @@ public class Config {
 
 
 //    // for jackson-databind
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/jackson-databind-2.19";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/jackson-databind-2.19";
 //    public static String package_name = "com.fasterxml.jackson.databind";
 //    public static String projectSrcDir = "/src/main/java/com/fasterxml/jackson/databind";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/com/fasterxml/jackson/databind";
@@ -32,7 +32,7 @@ public class Config {
 
 
 //    // for commons-collections
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/commons-collections-master";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-collections-master";
 //    public static String package_name = "org.apache.commons.collections4";
 //    public static String projectSrcDir = "/src/main/java/org/apache/commons/collections4";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/apache/commons/collections4";
@@ -40,14 +40,14 @@ public class Config {
 
 
 //    // for commons-compress
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/commons-compress-master";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-compress-master";
 //    public static String package_name = "org.apache.commons.compress";
 //    public static String projectSrcDir = "/src/main/java/org/apache/commons/compress";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/apache/commons/compress";
 //    public static String syntaxErrorLog = Experiment_Result_Basic_Dir + "/syntax_error_log.txt";
 
 //    // for commons-csv
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/commons-csv-master";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-csv-master";
 //    public static String package_name = "org.apache.commons.csv";
 //    public static String projectSrcDir = "/src/main/java/org/apache/commons/csv";
 //    public static String testCodeDir = Project_Basic_Dir + "src/test/java/org/apache/commons/csv";
@@ -56,14 +56,14 @@ public class Config {
 
 
 //    // for jackson-core
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/jackson-core-2.19";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/jackson-core-2.19";
 //    public static String package_name = "com.fasterxml.jackson.core";
 //    public static String projectSrcDir = "/src/main/java/com/fasterxml/jackson/core";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/com/fasterxml/jackson/core";
 //    public static String syntaxErrorLog = Experiment_Result_Basic_Dir + "/syntax_error_log.txt";
 
 //    // for jackson-dataformat-xml
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/jackson-dataformat-xml-2.19";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/jackson-dataformat-xml-2.19";
 //    public static String package_name = "com.fasterxml.jackson.dataformat.xml";
 //    public static String projectSrcDir = "/src/main/java/com/fasterxml/jackson/dataformat/xml";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/com/fasterxml/jackson/dataformat/xml";
@@ -71,7 +71,7 @@ public class Config {
 
 
 //    // for apache-commons-jxpath
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/commons-jxpath-master";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-jxpath-master";
 //    public static String package_name = "org.apache.commons.jxpath";
 //    public static String projectSrcDir = "/src/main/java/org/apache/commons/jxpath";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/apache/commons/jxpath";
@@ -79,7 +79,7 @@ public class Config {
 //
 
 //    // for joad-time
-//    public static String Project_Basic_Dir = "/Users/dianshuliao/Documents/Research/Knowledge_Distillation_for_LLM_Test_Gen/UnitTestGeneration/saved_data/TenJavaProjects/joda-time-main";
+//    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/joda-time-main";
 //    public static String package_name = "org.joda.time";
 //    public static String projectSrcDir = "/src/main/java/org/joda/time";
 //    public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/joda/time";
@@ -87,7 +87,7 @@ public class Config {
 
 
     // for commons-lang3
-    public static String ogn = BASE_DIR + "/experiment_results/TenProjects/commons-lang-master";
+    public static String Project_Basic_Dir = BASE_DIR + "/experiment_results/TenProjects/commons-lang-master";
     public static String package_name = "org.apache.commons.lang3";
     public static String projectSrcDir = "/src/main/java/org/apache/commons/lang3";
     public static String testCodeDir = Project_Basic_Dir + "/src/test/java/org/apache/commons/lang3";
